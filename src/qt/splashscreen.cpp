f2bc2d5549ca9254b09ebcf321bcea6e688f0ddf
MixAir <mixairdev@protonmail.com>
2018-08-31 02:15:12 +0300

    New version & fix splash

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -45,7 +45,7 @@ SplashScreen::SplashScreen(Qt::WindowFlags f, const NetworkStyle *networkStyle) 
     QString titleText       = tr("MIA Core");
     QString versionText     = QString(tr("Version %1")).arg(QString::fromStdString(FormatFullVersion()));
     QString copyrightTextBtc   = QChar(0xA9)+QString(" 2009-%1 ").arg(COPYRIGHT_YEAR) + QString(tr("The Bitcoin Core developers"));
-    QString copyrightTextMIA   = QChar(0xA9)+QString(" 2014-%1 ").arg(COPYRIGHT_YEAR) + QString(tr("The MIA Core developers"));
+    QString copyrightTextMIA   = QChar(0xA9)+QString(" ").arg(COPYRIGHT_YEAR) + QString(tr("The MIA Core developers"));
     QString titleAddText    = networkStyle->getTitleAddText();
     // networkstyle.cpp can't (yet) read themes, so we do it here to get the correct Splash-screen
     QString splashScreenPath = ":/images/" + GUIUtil::getThemeName() + "/splash";
